3f21c3dfa04535c90ec113f3dcf99a9c0b48f397
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-03-02 02:52:41 +0100

    Refactor duplicate sentinel

--- pkg/nostale/encoding/nsl/nsl.go
+++ pkg/nostale/encoding/nsl/nsl.go
@@ -11,17 +11,15 @@ func (e *Encoding) Decode(dst, src []byte) (ndst, nsrc int, err error) {
 	if len(dst) < len(src) {
 		panic("dst buffer is too small")
 	}
-	if len(src) == 0 {
-		return ndst, nsrc, nil
-	}
-	for ndst, nsrc = 0, 0; nsrc < len(src); ndst, nsrc = ndst+1, nsrc+1 {
-		if src[nsrc] > 14 {
-			dst[ndst] = (src[nsrc] - 15) ^ 195
+	var n int
+	for n = 0; n < len(src); n++ {
+		if src[n] > 14 {
+			dst[n] = (src[n] - 15) ^ 195
 		} else {
-			dst[ndst] = (255 - (14 - src[nsrc])) ^ 195
+			dst[n] = (255 - (14 - src[n])) ^ 195
 		}
 	}
-	return ndst, nsrc, nil
+	return n, n, nil
 }
 
 func (e *Encoding) DecodedLen(x int) int {
@@ -32,10 +30,11 @@ func (e *Encoding) Encode(dst, src []byte) (ndst, nsrc int, err error) {
 	if len(dst) < len(src) {
 		panic("dst buffer is too small")
 	}
-	for ndst, nsrc = 0, 0; nsrc < len(src); ndst, nsrc = ndst+1, nsrc+1 {
-		dst[ndst] = (src[nsrc] + 15) & 0xFF
+	var n int
+	for n = 0; n < len(src); n++ {
+		dst[n] = (src[n] + 15) & 0xFF
 	}
-	return ndst, nsrc, nil
+	return n, n, nil
 }
 
 func (e *Encoding) EncodedLen(x int) int {
