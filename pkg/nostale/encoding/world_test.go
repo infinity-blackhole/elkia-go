a7f518d81fcbd8b1c829d04aa9449857e1e6993a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-03-04 01:22:32 +0100

    Fix headbeat test

--- pkg/nostale/encoding/world_test.go
+++ pkg/nostale/encoding/world_test.go
@@ -5,15 +5,15 @@ import (
 	"testing"
 )
 
-// key:3324008792
-
 func TestWorldEncodingDecodeHandoffPasswordFrame(t *testing.T) {
 	input := []byte{
 		208, 203, 222, 209, 215, 208, 210, 218, 193, 112, 67, 220, 208, 210,
 		63, 199, 205, 171, 161, 16, 72, 215, 214, 221, 200, 214, 200, 214, 248,
 		193, 160, 65, 218, 193, 224, 66, 241, 205, 63, 10,
 	}
-	expected := []byte("49272 9hibwiwiG2e6Nr")
+	// The payload is "49272 9hibwiwiG2e6Nr", the rest is the keep alive
+	// and protocol garbage.
+	expected := []byte("475n5 5355-564 .com49272 9hibwiwiG2e6Nr\xca")
 	dec := NewDecoder(bytes.NewReader(input), WorldEncoding)
 	result := make([]byte, WorldEncoding.DecodedLen(len(input)))
 	if err := dec.Decode(&result); err != nil {
@@ -43,12 +43,12 @@ func TestWorldEncodingDecodeModeAndOffset(t *testing.T) {
 
 func TestWorldEncodingDecodeHeartbeatFrame(t *testing.T) {
 	input := []byte{
-		199, 205, 171, 241, 128, 63,
+		199, 205, 171, 241, 128, 63, 10,
 	}
 	expected := []byte("49277 0")
-	enc := NewDecoder(bytes.NewReader(input), WorldEncoding)
-	result := make([]byte, len(expected))
-	if err := enc.Decode(&result); err != nil {
+	dec := NewDecoder(bytes.NewReader(input), WorldEncoding)
+	result := make([]byte, WorldEncoding.DecodedLen(len(input)))
+	if err := dec.Decode(&result); err != nil {
 		t.Errorf("Error reading line: %s", err)
 	}
 	if !bytes.Equal(result, expected) {
